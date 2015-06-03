cac4c64aef6cffe7da625ff20513075964702a96
ShannonArmon <shannon.armon@gmail.com>
2015-06-03 17:53:09 -0400

    Take a Selfie add-ons
    added a filter

--- TakeASelfie/TakeASelfie/ViewController.m
+++ TakeASelfie/TakeASelfie/ViewController.m
@@ -7,17 +7,19 @@
 //
 
 #import "ViewController.h"
+#import "FilterCollectionViewCell.h"
 
-@interface ViewController ()
+@interface ViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
 
+@property (weak, nonatomic) IBOutlet UICollectionView *filterCollectionView;
 
 
 @end
 
 @implementation ViewController
+
 {
-
-
+    NSArray * filters;
 }
 
 - (void)viewDidLoad {
@@ -25,7 +27,55 @@
     
     self.imageView.image = self.original;
     
+    filters = [CIFilter filterNamesInCategory:kCICategoryColorEffect];
+    
+    self.filterCollectionView.dataSource = self;
+    
+    self.filterCollectionView.delegate = self;
+    
 }
 
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+
+    return filters.count;
+
+}
+
+
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    
+    FilterCollectionViewCell * cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"filterCell" forIndexPath:indexPath];
+    
+    NSString * filterName = filters[indexPath.item];
+    
+    cell.imageView.image = [self filterImage:self.original withName:filterName];
+    
+    
+    return cell;
+}
+
+- (UIImage *)filterImage:(UIImage *)originalImage withName:(NSString *)filterName {
+
+    CIContext * context = [CIContext contextWithOptions:nil];
+    
+    CIImage * image = [CIImage imageWithCGImage:originalImage.CGImage];
+    
+    CIFilter * filter = [CIFilter filterWithName:filterName];
+    
+    [filter setValue:image forKey:kCIInputImageKey];
+    
+//    [filter setValue:@0.8f forKey:kCIInputIntensityKey];
+    
+    CIImage * result = [filter valueForKey:kCIOutputImageKey];
+    
+    CGRect extent = [result extent];
+    
+    CGImageRef cgImage = [context createCGImage:result fromRect:extent];
+    
+    return [UIImage imageWithCGImage:cgImage];
+
+}
 
 @end
